5e03c5b0ebe4c177e3d20d5f77ee89bb5fb4dcb3
Swann <swanngille@gmail.com>
2026-02-22 03:49:06 +0100

    fix responsive title size

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -63,7 +63,7 @@ const Leaderboard = () => {
         >
             <Text
                 as="h1"
-                fontSize="4rem"
+                fontSize={{ base: '2.5rem', md: '4rem' }}
                 className={tuskerGrotesk.className}
             >
                 CLASSEMENT
