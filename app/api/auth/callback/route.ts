8fe83b30c4d52fa8e211d2bf4830c95c9a7214a3
Mitotow <abrs.thomas@gmail.com>
2026-05-13 16:59:30 +0200

    test: ajout d'une première batterie de test unitaires et suppression des console.log/error dans les routes api

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -33,7 +33,6 @@ export async function GET(request: NextRequest) {
 
         return NextResponse.redirect(redirection)
     } catch (error) {
-        console.error('Auth Error:', error)
         return NextResponse.json({ error: 'Authentication failed' }, { status: 500 })
     }
 }
