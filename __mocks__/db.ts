16618470cec64fe41d61ce8369e1429d76fc234c
Mitotow <abrs.thomas@gmail.com>
2026-05-16 12:15:23 +0200

    test: remove __mocks__

--- __mocks__/db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-import { beforeEach } from 'vitest'
-import { mockDeep, mockReset, DeepMockProxy } from 'vitest-mock-extended'
-
-export const prisma = mockDeep<PrismaClient>()
-
-beforeEach(() => {
-    mockReset(prisma)
-})
