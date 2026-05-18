044433f8e667284895f9724df29dae49f976600a
Mitotow <abrs.thomas@gmail.com>
2026-05-18 16:01:16 +0200

    feat: mise à jour du fonctionnement des attempts avec ajout d'une route /import pour importer plusieurs attempts d'un coup

---
+++ app/api/m8dle/attempt/import/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from 'next/server'
+import { prisma } from '@/lib/db'
+import { getSession } from '@/lib/auth/session'
+import { getGameDate } from '@/utils/dateUtils'
+import { MultipleAttemptDto } from '@/dto/AttemptDto'
+import { ZodError } from 'zod'
+
+export async function POST(req: Request) {
+    const session = await getSession()
+    if (!session?.userId) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+
+    try {
+        const body = await req.json()
+        const payload = MultipleAttemptDto.parse(body)
+
+        const gameDate = getGameDate()
+        const dailyResult = await prisma.dailyM8DLEResult.findUnique({
+            where: { userId_date: { userId: session.userId, date: gameDate } },
+            select: { id: true, success: true, attempts: true },
+        })
+
+        if (dailyResult) {
+            return NextResponse.json({ error: 'Daily result already exists' }, { status: 400 })
+        }
+
+        const players = await prisma.player.findMany({
+            where: {
+                id: {
+                    in: payload.attempts,
+                },
+            },
+        })
+
+        const attempts = []
+        for (let i = 0; i < players.length; i++) {
+            attempts.push({
+                playerId: players[i].id,
+                attemptNumber: i + 1,
+            })
+        }
+
+        const created = await prisma.dailyM8DLEResult.create({
+            data: {
+                userId: session.userId,
+                date: gameDate,
+                attempts: {
+                    createMany: {
+                        data: attempts,
+                    },
+                },
+            },
+            select: {
+                id: true,
+                success: true,
+                attempts: true,
+            },
+        })
+
+        return NextResponse.json(created, { status: 201 })
+    } catch (err) {
+        if (err instanceof ZodError) {
+            return NextResponse.json({ error: 'Bad Request' }, { status: 400 })
+        } else {
+            return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+        }
+    }
+}
