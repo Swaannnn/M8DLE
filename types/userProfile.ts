bfe6421b61653b450460f4b81fa5d28ad8dce73e
Mitotow <abrs.thomas@gmail.com>
2026-08-24 16:36:57 +0200

    fix: retour en arrière sur les dates dans UserProfile

--- types/userProfile.ts
+++ types/userProfile.ts
@@ -5,6 +5,6 @@ export type UserProfile = {
     email: string | null
     avatar: string | null
     role: string
-    createdAt: string
-    updatedAt: string
+    createdAt: Date
+    updatedAt: Date
 }
