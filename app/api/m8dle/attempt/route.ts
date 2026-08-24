3c48ee3a0ec69c956292a7c92c518d6b24299cfa
Mitotow <abrs.thomas@gmail.com>
2026-08-24 16:56:49 +0200

    fix-108: corrige le probleme de suppression d'attempts par des users non admin

--- app/api/m8dle/attempt/route.ts
+++ app/api/m8dle/attempt/route.ts
@@ -83,8 +83,10 @@ export async function POST(req: NextRequest) {
 export async function DELETE(req: NextRequest) {
     try {
         const session = await getSession()
-        if (!session?.userId && session?.role !== Role.ADMIN)
+        console.log(session?.role)
+        if (!session?.userId || session?.role !== Role.ADMIN) {
             return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+        }
 
         const id = req.nextUrl.searchParams.get('id')
         if (!id) {
