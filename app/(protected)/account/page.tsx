17d57a3485b0427bf2c517e92fd9a6d6419528f9
Swann <116310385+Swaannnn@users.noreply.github.com>
2026-08-24 21:10:55 +0200

    Feat/45/edit login page (#107)
    * feature-#69: add game recap in share text (#84)
    
    * feature-#69: add game recap in share text
    
    * feature-#69: improve share texts
    
    * fix-#86: suppression de la correction des warnings qui causer des problèmes avec la taille des images (#87)
    
    * 1.12.0 to 1.13.0
    
    * feat #45 : add login page in popup
    
    * feat #45 : fix indentation
    
    ---------
    
    Co-authored-by: Mitoto <34867561+Mitotow@users.noreply.github.com>

--- app/(protected)/account/page.tsx
+++ app/(protected)/account/page.tsx
@@ -43,7 +43,7 @@ const AccountPage = () => {
 
     useEffect(() => {
         if (!userLoading && loggedOut) {
-            router.push('/login')
+            router.push('/')
         }
     }, [userLoading, loggedOut, router])
 
